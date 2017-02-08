330601f4af3a1f157776a7e4e4250348df7ecb34
samuelzhouhe <hezhou556@gmail.com>
2017-02-08 15:57:03 +0800

    Add gitignore

--- CircleBrush.cpp
+++ CircleBrush.cpp
@@ -4,7 +4,6 @@
 // The implementation of Point Brush. It is a kind of ImpBrush. All your brush implementations
 // will look like the file with the different GL primitive calls.
 //
-
 #include "impressionistDoc.h"
 #include "impressionistUI.h"
 #include "CircleBrush.h"
