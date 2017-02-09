6ffa041eebbc23b2ff6448d52751f959a1e0f7c3
samuelzhouhe <hezhou556@gmail.com>
2017-02-09 21:21:45 +0800

    give proper name to new brush

--- ScatteredPointBrush.cpp
+++ ScatteredPointBrush.cpp
@@ -1,22 +1,21 @@
 //
-// CircleBrush.cpp
+// ScatteredPointBrush.cpp
 //
 // The implementation of Point Brush. It is a kind of ImpBrush. All your brush implementations
 // will look like the file with the different GL primitive calls.
 //
 #include "impressionistDoc.h"
 #include "impressionistUI.h"
-#include "CircleBrush.h"
-#include "CircleBrush.h"
+#include "ScatteredPointBrush.h"
 
 extern float frand();
 
-CircleBrush::CircleBrush(ImpressionistDoc* pDoc, char* name) :
+ScatteredPointBrush::ScatteredPointBrush(ImpressionistDoc* pDoc, char* name) :
 	ImpBrush(pDoc, name)
 {
 }
 
-void CircleBrush::BrushBegin(const Point source, const Point target)
+void ScatteredPointBrush::BrushBegin(const Point source, const Point target)
 {
 	ImpressionistDoc* pDoc = GetDocument();
 	ImpressionistUI* dlg = pDoc->m_pUI;
@@ -30,13 +29,13 @@ void CircleBrush::BrushBegin(const Point source, const Point target)
 	BrushMove(source, target);
 }
 
-void CircleBrush::BrushMove(const Point source, const Point target)
+void ScatteredPointBrush::BrushMove(const Point source, const Point target)
 {
 	ImpressionistDoc* pDoc = GetDocument();
 	ImpressionistUI* dlg = pDoc->m_pUI;
 
 	if (pDoc == NULL) {
-		printf("CircleBrush::BrushMove  document is NULL\n");
+		printf("ScatteredPointBrush::BrushMove  document is NULL\n");
 		return;
 	}
 
@@ -48,7 +47,7 @@ void CircleBrush::BrushMove(const Point source, const Point target)
 	glEnd();
 }
 
-void CircleBrush::BrushEnd(const Point source, const Point target)
+void ScatteredPointBrush::BrushEnd(const Point source, const Point target)
 {
 	// do nothing so far
 }
